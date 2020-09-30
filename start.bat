cf8f9e8ee7035d189564568eb91657212c57de02
eric-robertson <e.robertson.js@gmail.com>
2020-09-30 19:29:13 -0400

    fix 2

--- start.bat
+++ start.bat
@@ -1,3 +1,1 @@
-call cd server
-java -DIReallyKnowWhatIAmDoingISwear -Xmx1024M -Xms1024M -jar spigot-1.16.2.jar
-
+java -DIReallyKnowWhatIAmDoingISwear -Xmx1024M -Xms1024M -jar ./server/spigot-1.16.2.jar
